fa4bfd206d86368043fc9e319dde99bac3e25da5
walidloubar <walidloubar@gmail.com>
2021-12-27 16:25:42 +0100

    miaw

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
-const expressLayouts = require ('express-ejs-layouts')
+const express = require('express');
+const app = express();
+const expressLayouts = require ('express-ejs-layouts');
+const indexRouter = require('./routes/index');
 
-app.set('viewengine','ejs')
+app.use(expressLayouts);
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.use('/',indexRouter);
+
+app.listen(process.env.PORT || 3000);
